e3e75c9644d47200cc0892d355cbeead15c5e32c
Leonard Akpofure <la.shivure@gmail.com>
2024-01-11 14:22:51 +0100

    Add classnames package and update NavBar component

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,12 @@
+'use client';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { ImBug } from "react-icons/im";
+import classnames from 'classnames'
 
 const NavBar = () => {
+    const currentPath = usePathname()
 
     const links = [ 
         { label: 'Dashboard', href: '/' },
@@ -12,8 +16,9 @@ const NavBar = () => {
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href="/"><ImBug /></Link>
         <ul className='flex space-x-6'>
-            {links.map(link => <Link key={link.href} 
-            className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+            {links.map(link => <Link 
+            key={link.href} 
+            className={`${link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800 transition-colors`} 
             href={link.href}>{link.label}</Link> )}
         </ul>
     </nav>
